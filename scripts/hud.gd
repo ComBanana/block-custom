5a3220e2bb80970be50de34cb00ce824caa90c17
ComBanana <140673792+ComBanana@users.noreply.github.com>
2026-09-21 20:05:25 +0800

    Display game version in HUD

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -6,6 +6,7 @@ extends CanvasLayer
 
 @onready var player: CharacterBody3D = $"../Player"
 @onready var run_icon: TextureRect = $RunIcon
+@onready var version_label: Label = $VersionLabel
 
 
 func _process(_delta: float) -> void:
@@ -20,3 +21,7 @@ func _process(_delta: float) -> void:
 	]
 
 	run_icon.visible = GameSettings.toggle_sprint or player.running
+	version_label.text = "v%s" % ProjectSettings.get_setting(
+		"application/config/version",
+		"0.1.0"
+	)
